07eedd1214876f9cad93a02537cc510940ebf80d
GuoJingYuan <1973589655@qq.com>
2022-05-26 14:31:07 +0800

    delete shiyan(bu yong guan zhu)

--- shyan/PokemonManage.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include"Pokemon.h"
-#include<string>
-class PokemonManage
-{
-public:
-	static Pokemon* getPokemon(std::string name);
-};
-
--- shyan/源.cpp
+++
@@ -1,7 +0,0 @@
-#include "DangerException.h"
-#include<iostream>
-void DangerException::ShowError(int OverweightWeight) {
-	std::cout << "超重" << std::endl;
-	std::cout << "无法再装载重量是" << OverweightWeight << "吨重的集装箱" << std::endl;
-	std::cout << "货船将正点启航" << std::endl;
-}
